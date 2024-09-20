4872e317f0a537d81afd79abcf0fced43eb80b4b
sajjadahmad18108 <sajjadahmad18108@gmail.com>
2024-09-20 09:25:53 +0500

    Post request

--- index.mjs
+++ index.mjs
@@ -23,6 +23,20 @@ const server = createServer((req, res)=>{
         res.writeHead(200, { 'Content-Type': 'text/plain' });  
         res.end("This is the contact page");  
     }
+
+    // Post request 
+    else if (parseurl.pathname === '/user' && req.method === 'POST') {
+        let body = ''
+        req.on('data', chunk=>{
+            body += chunk.toString();
+        })
+        req.on('end', ()=>{
+            const newItems = JSON.parse(body)
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({message: "Post request user added successfull" , data:newItems}))
+        })
+     
+    } 
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end("Page not found");
